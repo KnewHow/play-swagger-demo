8f8a346ab68a765c026b33de3b47f28f34a4d297
KnewHow <948170910@qq.com>
2018-05-10 16:20:36 +0800

    初步完成action注解和属性注解解析

--- app/controllers/ExampleController.scala
+++ app/controllers/ExampleController.scala
@@ -9,12 +9,49 @@ import scala.concurrent._
 import com.dripower.play.swa._
 import org.joda.time._
 import play.swagger.annotation._
+import scala.annotation.meta
 
-case class Car(id:Long,logo:String)
-case class PersonGet(id: Long)
-case class Person(id: Long, name: String, age: Int,car: Car,level1ExpiredAt: Option[DateTime] = None,avatar: Option[String] = None,gmtCreate:DateTime = DateTime.now(),gmtModified:DateTime = DateTime.now(),friends:List[Friend] = List())
-case class Friend(id:Long,name:String,relation:String)
-case class Ids(ids:List[Long],op:Option[String])
+case class Car(
+  @(FieldAnnotation @meta.getter)(descrip="汽车ID")
+    id:Long,
+  @(FieldAnnotation @meta.getter)(descrip="汽车标志")
+    logo:String
+)
+case class PersonGet(
+  @(FieldAnnotation @meta.getter)(descrip="人的ID")
+  id: Long
+)
+case class Person(
+  @(FieldAnnotation @meta.getter)(descrip="人的ID")
+    id: Long,
+   @(FieldAnnotation @meta.getter)(descrip="人的名字")
+     name: String,
+  @(FieldAnnotation @meta.getter)(descrip="人的年龄")
+    age: Int,
+  @(FieldAnnotation @meta.getter)(descrip="人所属的汽车")
+    car: Car,
+  @(FieldAnnotation @meta.getter)(descrip="测试时间戳")
+    level1ExpiredAt: Option[DateTime] = None,
+  @(FieldAnnotation @meta.getter)(descrip="人的头像")
+    avatar: Option[String] = None,
+  @(FieldAnnotation @meta.getter)(descrip="创建时间")
+    gmtCreate:DateTime = DateTime.now(),
+  @(FieldAnnotation @meta.getter)(descrip="修改时间")
+     gmtModified:DateTime = DateTime.now(),
+  @(FieldAnnotation @meta.getter)(descrip="所属的朋友")
+    friends:List[Friend] = List()
+)
+case class Friend(
+  @(FieldAnnotation @meta.getter)(descrip="朋友的ID")
+    id:Long,
+  @(FieldAnnotation @meta.getter)(descrip="朋友的名称")
+    name:String,
+  @(FieldAnnotation @meta.getter)(descrip="关系")
+    relation:String
+)
+case class Ids(ids:List[Long],
+  op:Option[String]
+)
 
 object PersonGet {
   implicit val format = Json.format[PersonGet]
@@ -35,6 +72,12 @@ object Person {
 class ExampleController  @Inject() (val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController {
 
   val Swa = SwaActionBuilder(Action)
+
+  @ActionAnnotation(descrip="不是正式的请求")
+  def noFormatAction:PostSwaAction [PersonGet, Person] = Swa.asyncPost[PersonGet,Person](parse.json[PersonGet]) { req =>
+    val personGet = req.body
+    Future.successful(Person(personGet.id, "foo", 1,Car(1L,"奔驰")))
+  }
 
   @ActionAnnotation(descrip="测试 POST 请求")
   def examplePostAction:PostSwaAction [PersonGet, Person] = Swa.asyncPost[PersonGet,Person](parse.json[PersonGet]) { req =>
