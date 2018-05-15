09f033b3dff82ec114b3febe9b3950a349065cac
KnewHow <948170910@qq.com>
2018-05-15 17:25:16 +0800

    页面切割-api-请求参数显示

--- client/src/components/play-swa/directory.vue
+++ client/src/components/play-swa/directory.vue
@@ -16,7 +16,7 @@
           </div>
           <div class="second-cate" :id="'second-cate-' + index">
             <div v-for="(seName, seIndex) in abstractSecondCate(index)" :key="seIndex">
-              <div class="second-cate-name" @click="expandThirdCategory(index,seIndex)">
+              <div class="second-cate-name" @click="expandThirdCategory(index,seIndex)" :tabindex="seIndex">
                 <span>
                   {{index+1}}.{{seIndex+1}}
                 </span>
@@ -26,7 +26,7 @@
               </div>
               <div class="third-cate" :id="'third-cate-' + index + seIndex">
                 <div v-for="(thName,thIndex) in abstractThirdCate(index,seIndex)" :key="thIndex">
-                  <div class="third-cate-name">
+                  <div class="third-cate-name" :tabindex="thIndex" @click="showApiDatail(index, seIndex, thIndex)">
                     <span>
                       {{index+1}}.{{seIndex+1}}.{{thIndex+1}}
                     </span>
@@ -42,7 +42,46 @@
       </div>
 
     </div>
-    <div class="detail">
+    <div class="detail" v-if="isShowDetail">
+      <div class="overview">
+        <div class="descrip">{{api.descrip}}</div>
+        <div class="route">
+          <span>路由：</span>
+          <span>{{api.route}}</span>
+        </div>
+        <div class="method">
+          <span>
+            请求方式：
+          </span>
+          <span>
+            {{api.method}}
+          </span>
+        </div>
+      </div>
+
+      <div class="parameters">
+        <div class="request">
+          <div>请求参数列表:</div>
+          <div class="list">
+            <table class="table" border="1">
+              <tr>
+                <th>参数名称</th>
+                <th>参数类型</th>
+                <th>描述</th>
+              </tr>
+              <tr v-for="(v,k) in api.req" :key="k">
+                <td>{{k}}</td>
+                <td>{{v.type}}</td>
+                <td>{{v}}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+
+        <div class="response">
+
+        </div>
+      </div>
     </div>
 
   </div>
@@ -57,7 +96,9 @@ export default {
       firstCate: null,
       secondCate: null,
       secondCateFlag: null,
-      thirdCateFlag: null
+      thirdCateFlag: null,
+      api: Object,
+      isShowDetail: false
     }
   },
   methods: {
@@ -132,6 +173,14 @@ export default {
         document.getElementById('third-cate-' + fi + si).style.display = 'block'
       }
     },
+    showApiDatail (fi, si, ti) {
+      var se = this.secondCate[fi][si]
+      for (var k in se) {
+        var thirdCateArr = se[k]
+        this.api = thirdCateArr[ti]
+      }
+      this.isShowDetail = true
+    },
     isShowSecondCate (index) {
       console.log('is' + (this.secondCateFlag[index] === true))
       return this.secondCateFlag[index] === true
@@ -144,17 +193,25 @@ export default {
 </script>
 
 <style scoped>
+.content:after {
+  content: " ";
+  display: block;
+  clear: both;
+  height: 0;
+}
+.content {
+  zoom: 1;
+}
 .content {
   position: absolute;
   background-color: #fff;
-  border: 1px solid #dadada;
   margin-top: 4px;
   height: 82%;
-  border-right: 5px solid #dadada;
 }
 .directory {
   width: 300px;
   float: left;
+  border-right: 5px solid #dadada;
 }
 .d-name {
   text-align: center;
@@ -188,8 +245,54 @@ export default {
 }
 div:focus {
   background-color: rgb(233, 104, 107);
+  color: #fff;
+  border: 0px solid #dadada;
 }
 .detail {
+  padding-top: 10px;
   float: left;
+}
+.overview {
+  padding-left: 30px;
+  width: 900px;
+}
+.descrip {
+  text-align: center;
+  font-size: 30px;
+}
+.route {
+  margin: 50px 0px 10px 0px
+}
+.method {
+  margin: 10px 0px 10px 0px;
+}
+.parameters {
+  padding-left: 30px;
+}
+.table {
+  font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  margin-top: 10px;
+  width:500px;
+}
+.parameters table {
+  border: 1px solid black;
+}
+.parameters th, .parameters td {
+  font-size: 14px;
+  border:1px solid #000;
+  padding:3px 7px 2px 7px;
+}
+.parameters th {
+  font-size: 14px;
+  text-align: center;
+  padding-top: 5px;
+  padding-bottom: 4px;
+  color: #fff;
+  background-color: green;
+}
+
+.h-show {
+  margin:0px 20px 0px 10px
 }
 </style>
